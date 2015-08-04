d69a1daa87bae379b2c3ec5125dacacbadf8fa97
jackyon <jackyon@jackyondeMacBook-Pro.local>
2015-08-04 11:01:23 +0800

    updates

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,10 @@ require.config({
     paths: {
         jquery: 'libs/jquery/jquery.min'
     }
+    // shim: {
+    // 	fullPage: ['jquery'],
+    //     tiltFx: ['jquery']
+    // }
 });
 
 require(['models/example/main'], function(example){
--- app/js/models/example/main.js
+++ app/js/models/example/main.js
@@ -1,3 +1,18 @@
+// only for test
+// requirejs.config({
+//     baseUrl: '../',
+//     paths: {
+//         jquery: 'js/libs/jquery/jquery.min',
+//         tiltFx: 'js/models/tilt_fx/tilt_fx'
+//     },
+//     shim: {
+//         tiltFx: {
+//             deps: ['jquery']
+//         }
+//     }
+// });
+
+
 define(['jquery'], function($){
     return {
         init: function(){

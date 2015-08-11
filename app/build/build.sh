b4dfe97e4aaf82fbea77861205917633cccd81d4
jackyon <jackyon@jackyondeMacBook-Pro.local>
2015-08-11 22:54:27 +0800

    build update

--- app/build/build.sh
+++ app/build/build.sh
@@ -1,6 +1,6 @@
 r.js -o build/app.build.js
 cd ../dist
 mv js/libs/requirejs/require.js require.js
-rm -rf js/libs/* build js/views js/models js/collections build.txt
+rm -rf js/libs/* build js/views js/models js/collections js/plugins/ build.txt test
 mkdir js/libs/requirejs && mv require.js js/libs/requirejs/require.js
 # mv css/styles.css styles.css && rm -rf css/* && mv styles.css css/styles.css
